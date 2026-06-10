41e3b0f54fbb6ee5a893c8dfb8fd6580bc96ed45
Jose <joseaguilarsalazar2004@gmail.com>
2026-06-10 14:17:21 -0500

    added urls to main ones

--- core/urls.py
+++ core/urls.py
@@ -17,5 +17,5 @@ router.register(r'attendance', AttendanceViewSet, basename='attendance')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('/', include(router.urls)),
+    path('', include(router.urls)),
 ]
